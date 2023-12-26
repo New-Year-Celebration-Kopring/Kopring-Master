5ba31d2145417184700d7b4a4a2d40fcb26e1ae2
ontheway-01 <eunhwa6087@naver.com>
2023-12-26 15:42:39 +0900

    modify dependencies, create Controller

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,20 +24,20 @@ repositories {
 val snippetsDir by extra {file("build/generated-snippets")}
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-cache")
+	//implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	//implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	//implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	//implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-	testImplementation("org.springframework.security:spring-security-test")
+	//testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	//testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
@@ -51,7 +51,7 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-tasks.test {
+/*tasks.test {
 	outputs.dir(snippetsDir)
 }
 
@@ -59,4 +59,4 @@ tasks.asciidoctor {
 	inputs.dir(snippetsDir)
 	//dependsOn(test)
 	dependsOn(tasks.test)
-}
+}*/
